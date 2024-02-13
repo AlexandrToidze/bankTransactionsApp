dd1d2a6468ca91750e1dfd1e4a67bc1922971a1c
Alexander Toidze <ato@aiweapps.com>
2024-02-13 13:18:33 +0300

    fill transactions history

--- app/src/main/java/com/raremode/bankapp/repository/TransactionsHistory.kt
+++ app/src/main/java/com/raremode/bankapp/repository/TransactionsHistory.kt
@@ -12,17 +12,23 @@ class TransactionsHistory() {
 
             TransactionHistoryModel(
                 service = "http://figma.com/",
-                sum = (201.0)
+                sum = (201.74),
+                type = "Job",
+                transactionDate = "February 13, 12:43 a.m."
             ),
 
             TransactionHistoryModel(
                 service = "http://vk.com/",
-                sum = (379.0)
+                sum = (379.21),
+                type = "Entertainments",
+                sumSubtitle = "BBank credit",
+                transactionDate = "February 13, 12:43 a.m."
             ),
 
             TransactionHistoryModel(
                 service = "http://google.com/",
-                sum = (520.0)
+                sum = (520.89),
+                transactionDate = "February 13, 12:43 a.m."
             ),
 
             TransactionHistoryDateModel(
@@ -30,28 +36,55 @@ class TransactionsHistory() {
             ),
 
             TransactionHistoryModel(
-                service = "http://facebook.com/",
-                sum = (-800.0)
+                service = "http://ozon.com/",
+                sum = (-852.10),
+                transactionDate = "February 12, 19:21 a.m."
+
             ),
 
             TransactionHistoryModel(
                 service = "http://figma.com/",
-                sum = (201.0)
+                sum = (201.40),
+                transactionDate = "February 12, 19:21 a.m."
             ),
 
             TransactionHistoryModel(
                 service = "http://vk.com/",
-                sum = (379.0)
+                sum = (379.29),
+                type = "Job",
+                transactionDate = "February 12, 19:21 a.m."
             ),
 
             TransactionHistoryModel(
                 service = "https://vk.com/",
-                sum = (520.0)
+                sum = (520.73),
+                transactionDate = "February 12, 19:21 a.m."
             ),
 
             TransactionHistoryModel(
-                service = "http://facebook.com/",
-                sum = (-800.0)
+                service = "http://apple.com/",
+                sum = (-80000.0),
+                type = "Technic",
+                transactionDate = "February 12, 18:22 a.m."
+
+            ),
+
+            TransactionHistoryDateModel(
+                date = "Later"
+            ),
+
+            TransactionHistoryModel(
+                service = "http://figma.com/",
+                sum = (589.12),
+                type = "Job",
+                transactionDate = "February 13, 12:43 a.m."
+            ),
+
+            TransactionHistoryModel(
+                service = "http://ozon.com/",
+                sum = (-1094.71),
+                transactionDate = "February 12, 19:21 a.m."
+
             ),
         )
     }
